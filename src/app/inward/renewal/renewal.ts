a903b1e5a2dd2029a3b8c9f51eab4e730b0f1431
Mayur Kohad <mayur.kohad@nusummit.com>
2026-07-30 11:42:25 +0530

    sidebar updated

---
+++ src/app/inward/renewal/renewal.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-renewal',
+  imports: [],
+  templateUrl: './renewal.html',
+  styleUrl: './renewal.css',
+})
+export class Renewal {
+
+}
